03cc8bc81acee58c9116bc003d9f3f01d0ca7538
Steve Jones <jones.wiredbraincoffee@gmail.com>
2019-10-26 03:39:25 -0700

    Added __init__ methods to classes

--- Course.py
+++ Course.py
@@ -1,3 +1,8 @@
 class Course:
 	""" Class for defining Course objects """
+	def __init__(self, name, instructor, section, time):
+		self.name = name
+		self.instructor = instructor
+		self.section = section
+		self.time = time
 	
--- Student.py
+++ Student.py
@@ -1,3 +1,8 @@
 class Student:
     """Student class"""
-    def __init__:
+    def __init__(self, name, id_num, res_hall, schedule):
+    	self.name = name
+    	self.id_num = id_num
+    	self.res_hall = res_hall
+    	self.schedule = schedule
+    
